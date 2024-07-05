12aae2c3a73f27baa25771842601883fa9feec44
COLLINS TONUI <tckiprotich@outlook.com>
2024-07-05 16:46:44 +0000

    chore: Remove unnecessary webpack hot-update files and configurations

--- server/pages/api/products/index.ts
+++
@@ -1,6 +0,0 @@
-import {NextApiRequest, NextApiResponse} from "next";
-import {products} from "../../index";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    res.status(200).json(products);
-}
